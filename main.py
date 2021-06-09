dfe5f4ce17c74a0c5fd71b89ec92bed31b3e560d
David Winch <d.winchy+git@gmail.com>
2021-06-09 18:33:37 +0100

    Added minor logging

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+import logging
 import random
 from typing import Optional
 
-from fastapi import FastAPI, Response, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
 
 import clipfetcher
+
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
 
 app = FastAPI()
 
@@ -40,15 +55,20 @@ def read_item():
 """
 
 @app.get("/soclip/{username}")
-def read_item(username: str):
+def soclip(username: str):
+    logger.info(f"Running soclip function for user {username}")
     username = username.replace("@", "")
     access_token = clipfetcher.get_auth()
     broadcaster_id = clipfetcher.get_broadcaster_id(access_token, username)
     if not broadcaster_id:
+        logger.warning(f"Could not find broadcaster_id for {username}")
         raise HTTPException(status_code=404, detail=f"Error getting broadcaster_id for {username}")
     clips = clipfetcher.get_broadcaster_clips(access_token, broadcaster_id, first=100)
     if not clips:
+        logger.warning(f"Could not find any clips for {username}")
         raise HTTPException(status_code=404, detail=f"No clips found for {username}")
+    logger.info(f"Found {len(clips)} clip(s) for {username}")
     clip = random.choice(clips)
     mp4_url = clipfetcher.get_clip_url(access_token, clip['url'])
+    logger.info(f"Returning clip {mp4_url} for {username}")
     return Response(content=mp4_url, media_type="text/plain")
